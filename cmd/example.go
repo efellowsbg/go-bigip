7ee34a90d810f7ac9e889621632e4a908ef904d7
Blagoy Simandoff <bsimandov@efellows.bg>
2024-07-22 14:16:26 +0300

    added ilx structs and a get method for workspaces

---
+++ cmd/example.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/f5devcentral/go-bigip"
+)
+
+func main() {
+	// Connect to the BIG-IP system.
+	config := bigip.Config{
+		Address:           os.Getenv("BIG_IP_HOST"),
+		Username:          os.Getenv("BIG_IP_USER"),
+		Password:          os.Getenv("BIG_IP_PASSWORD"),
+		CertVerifyDisable: true,
+	}
+
+	f5 := bigip.NewSession(&config)
+	result, err := f5.GetWorkspace("sso")
+	if err != nil {
+		panic(err)
+	}
+	spew.Dump(result)
+}
